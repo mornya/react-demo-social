013eb0dc6f5d3fec248766c9fdcaba99f5cd5355
mornya <mornya78@gmail.com>
2020-03-08 20:07:21 +0900

    Updates

---
+++ App.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import '@mornya/react-social-libs/dist/res/share.css';
+import './App.scss';
+export default function App(): JSX.Element;
---
+++ constants/configure.d.ts
@@ -0,0 +1,2 @@
+declare const _default: IConfigure;
+export default _default;
